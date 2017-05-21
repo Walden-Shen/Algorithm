fb1b4b632fd002fa126ce6ba8a311a342d974902
Walden-Shen <41880356@qq.com>
2017-05-21 16:18:04 +0800

    kruscal mst

---
+++ algorithm/chapter4/KruskalMST.java
@@ -0,0 +1,24 @@
+public class KruscalMST{
+	private LinkedList<Edge> mst;
+	public KruscalMST(EdgeWeightedGraph G){
+		mst = new LinkedList<Edge>();
+		MinPQ<Edge> pq = new MinPQ<>();
+		UF uf = new UF(G.V());
+		for(Edge e : G.edges())
+			pq.insert(e);
+		while(!pq.isEmpty() && mst.size() < G.V() - 1){
+			Edge e = pq.delMin();
+			int v = e.either();
+			int w = e.other(v);
+			if(!uf.isConnected(v, w){
+				uf.union(v, w);
+				mst.offer(e);
+			}
+		}
+	}
+	public Iterable<Edge> edges{
+		return mst;
+	}
+	public double weight()
+
+
